93bed9090d7454eb2de8f770a2a3e0405f6a1b03
Samuel <samuelucas708@gmail.com>
2026-09-08 22:05:49 -0300

    Atualização do autentucar.php

--- autenticar.php
+++ autenticar.php
@@ -1,1 +1,22 @@
-    
+<?php
+
+include 'conexao.php';
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$sql = "SELECT * FROM clientes WHERE senha = '$senha' AND email = '$email'";
+
+$resultado = mysqli_query(
+    $conexao,
+    $sql
+);
+
+if(mysqli_num_rows($resultado) > 0){
+    echo "Login realizado com sucesso!";
+}else{
+    echo "Email ou senha inválidos! Tente novamente!\n";
+    echo "<a href='login.html'>VOLTAR</a>";
+}
+
+?> 
